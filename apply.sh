1182c014ea0591a5e3475d3d06e9fca807343e12
PattenChiou <patten1719@gmail.com>
2024-12-09 14:56:24 +0800

    combine apply.sh

--- apply.sh
+++ apply.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
 kubectl delete svc -n istio-system istio-ingressgateway
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 20; done
 kubectl patch svc -n istio-system istio-ingressgateway -p '{"spec":{"externalIPs":["120.126.23.25"]}}'
 # kubectl patch svc -n istio-system istio-ingressgateway -p '{"spec":{"type":"NodePort"}}'
+git clone https://github.com/wycc/cgu-manifests.git -b cgu-1.8
+while ! kustomize build cgu-manifests/example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+kubectl taint nodes $(hostname -s) node-role.kubernetes.io/control-plane:NoSchedule
+helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
+helm repo update
+VER=0.15.1
+helm upgrade -i nvdp nvdp/nvidia-device-plugin \
+  --version $VER \
+  --namespace nvidia-device-plugin \
+  --create-namespace \
+  --set compatWithCPUManager=true \
+  --set gfd.enabled=true
