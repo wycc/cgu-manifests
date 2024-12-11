a3fb4dbf1c7b1d6859ed81261e4dd29569c60e19
PattenChiou <patten1719@gmail.com>
2024-12-11 16:10:05 +0800

    fix apply.sh

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+cd cgu-manifests
 kubectl delete svc -n istio-system istio-ingressgateway
-while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 20; done
-kubectl patch svc -n istio-system istio-ingressgateway -p '{"spec":{"externalIPs":["120.126.23.25"]}}'
+while ! kustomize build ./example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 20; done
+kubectl patch svc -n istio-system istio-ingressgateway -p '{"spec":{"externalIPs":["192.168.1.235"]}}'
 # kubectl patch svc -n istio-system istio-ingressgateway -p '{"spec":{"type":"NodePort"}}'
-while ! kustomize build ./example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
 kubectl taint nodes $(hostname -s) node-role.kubernetes.io/control-plane:NoSchedule
 helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
 helm repo update
