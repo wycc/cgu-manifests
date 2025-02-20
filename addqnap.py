9899b9e43729e5129c0b6a8aa7e2b1672b2d8606
root <wycca1@gmail.com>
2025-02-20 11:13:07 +0800

    read configuration from env.ini

--- addqnap.py
+++ addqnap.py
@@ -1,17 +1,38 @@
 import os
 import base64
 
-yaml="""
+f=open('env.ini')
+ip = ''
+username=''
+password=''
+for l in f.read().split("\n"):
+    ff=l.split('=')
+    if ff[0] == 'QNAP_IP':
+        ip = ff[1].strip()
+    elif ff[0] == 'QNAP_USERNAME':
+        username = ff[1].strip()
+    elif ff[0] == 'QNAP_PASSWORD':
+        password = ff[1].strip()
+
+if ip =='' or username=='' or password=='':
+    print("QNAP configuration is not defined")
+    os.exit(-1)
+
+ip = base64.b64encode(ip.encode('UTF-8')).decode('UTF-8')
+username = base64.b64encode(username.encode('UTF-8')).decode('UTF-8')
+password = base64.b64encode(password.encode('UTF-8')).decode('UTF-8')
+
+yaml=f"""
 apiVersion: v1
 data:
-  ip: MTAuMTAwLjQuNzE=
-  password: UEBzc3cwcmQ=
-  username: cW5hcHVzZXI=
+  ip: {ip}
+  password: {password}
+  username: {username}
 kind: Secret
 metadata:
   name: qnap
 """
-
+print(yaml)
 os.system("kubectl get profile > /tmp/users.txt")
 
 f=open("/tmp/users.txt","r")
