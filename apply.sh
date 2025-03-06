f064f63c104a37de8e953ec078cc4bbd58bac5cc
root <wycca1@gmail.com>
2025-03-06 08:49:18 +0800

    Add openwebui port 33222

--- apply.sh
+++ apply.sh
@@ -28,7 +28,7 @@ cd cgu-manifests
 kubectl delete svc -n istio-system istio-ingressgateway
 while ! kustomize build ./example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 20; done
 kubectl patch svc -n istio-system istio-ingressgateway -p "{\"spec\":{\"externalIPs\":[\"${MASTER_IP}\"]}}"
-# kubectl patch svc -n istio-system istio-ingressgateway -p '{"spec":{"type":"NodePort"}}'
+# kubectl patch svc -n istio-system istio-ingressgateway -p '{"spec":{"type":"ClusterIP"}}'
 kubectl taint nodes $(hostname -s) node-role.kubernetes.io/control-plane:NoSchedule
 helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
 helm repo update
