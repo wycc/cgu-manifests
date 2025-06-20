bc38fbeb12206bca775c8a62645fc3bbf840da7f
Lance Lu <lance.cl.lu@gmail.com>
2025-06-20 20:49:01 +0800

    Add addqnap.py

--- addqnap.py
+++ addqnap.py
@@ -22,7 +22,15 @@ ip = base64.b64encode(ip.encode('UTF-8')).decode('UTF-8')
 username = base64.b64encode(username.encode('UTF-8')).decode('UTF-8')
 password = base64.b64encode(password.encode('UTF-8')).decode('UTF-8')
 
-yaml=f"""
+os.system("kubectl get profile > /tmp/users.txt")
+
+f=open("/tmp/users.txt","r")
+lines = f.read().split("\n")
+for l in lines[1:]:
+    ff = l.split(" ")
+    namespace = ff[0]
+    if namespace == '': break
+    yaml=f"""
 apiVersion: v1
 data:
   ip: {ip}
@@ -31,21 +39,31 @@ data:
 kind: Secret
 metadata:
   name: qnap
-"""
-print(yaml)
-os.system("kubectl get profile > /tmp/users.txt")
+  namespace: {namespace}
+---
+apiVersion: v1
+kind: LimitRange
+metadata:
+  name: default-cpu-mem-limits
+  namespace: {namespace}
+spec:
+  limits:
+  - type: Container
+    defaultRequest:
+      cpu: "250m"  # 設定預設 CPU requests
+      memory: "512Mi"  # 設定預設 Memory requests
+    default:
+      cpu: "500m"  # 設定預設 CPU limits
+      memory: "1Gi"  # 設定預設 Memory limits
 
-f=open("/tmp/users.txt","r")
-lines = f.read().split("\n")
-for l in lines[1:]:
-    ff = l.split(" ")
-    u = ff[0]
-    cmd = yaml+"\n  namespace: "+u+"\n"
+"""
     f=open("/tmp/tmp.yaml","w")
     f.write(yaml)
-    f.write("\n  namespace: "+u+"\n")
+    #f.write("\n  namespace: "+u+"\n")
     f.close()
-    print("add qnap to "+u+"\n")
+    print("add qnap to "+namespace+"\n")
+    #print(yaml)
+    #os.exit(0)
     os.system("kubectl apply -f /tmp/tmp.yaml")
     
 
